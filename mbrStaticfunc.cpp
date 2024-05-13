b2ead3f1597ba996ee0b674a98daabe98bca664b
rnktlhr <rinakitluhur10@gmail.com>
2024-05-13 15:14:18 +0700

    menambahkan main mahasiswa

--- mbrStaticfunc.cpp
+++ mbrStaticfunc.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-class mahasiswa{
+class Mahasiswa{
     private:
         static int nim;
     public:
@@ -15,23 +15,36 @@ class mahasiswa{
         static void setNim(int pNim) {nim = pNim;}
         static int getNim() {return nim;}
 
-        mahasiswa(string pnama):nama(pnama){
+        Mahasiswa(string pnama):nama(pnama){
             setID();
         }
 };
 
-int mahasiswa::nim = 100;
+int Mahasiswa::nim = 100;
 
-void mahasiswa::setID(){
+void Mahasiswa::setID(){
     id = ++nim;
 }
 
-void mahasiswa::prinALL(){
+void Mahasiswa::prinALL(){
     cout << "ID    = " << id <<endl;
     cout << "Nama  = " << nama <<endl;
     cout << endl;
 }
 
 int main(){
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
 
+    Mahasiswa::setNim(9);
+
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.prinALL();
+    mhs2.prinALL();
+    mhs3.prinALL();
+    mhs4.prinALL();
+
+    return 0;
 }
