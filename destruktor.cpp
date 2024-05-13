dd8cff5ab355804f617a60fa2aa60b1770a439ea
rnktlhr <rinakitluhur10@gmail.com>
2024-05-13 18:09:07 +0700

    membernarkan dan menambahkan void angka isi data

--- destruktor.cpp
+++ destruktor.cpp
@@ -28,12 +28,17 @@ angka::~angka(){
 
 void angka::cetakData(){
     for(int i = 1; i <= panjang; i++){
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData(){
+    for(int i = 1; i <= panjang; i++){
         cout << i << " = ";
         cin >> arr[i];
     }
     cout << endl;
 }
-
 int main(){
 
 }
