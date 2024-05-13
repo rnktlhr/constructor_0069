67d72e92aa9907a6ff74c8e38636236aa740f44f
rnktlhr <rinakitluhur10@gmail.com>
2024-05-13 14:39:25 +0700

    menambahkan member dan function pada class mahasiswa

--- mbrStatic.cpp
+++ mbrStatic.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 class mahasiswa{
-    
+    public:
+        static int nim;
+        int id;
+        string nama;
+
+        void setID();
+
+
+        void prinALL();
+        mahasiswa(string pnama):nama(pnama){
+            setID();
+        }
+
 };
 
 int main(){
