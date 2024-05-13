b14fe83cd31df7f51c803732abe802e8dba6216c
rnktlhr <rinakitluhur10@gmail.com>
2024-05-13 18:00:24 +0700

    menambahkan class beserta member dan function serta set angka

--- destruktor.cpp
+++ destruktor.cpp
@@ -2,6 +2,24 @@
 #include <string>
 using namespace std;
 
+class angka{
+    private:
+        int *arr;
+        int panjang;
+    public:
+        angka(int);
+        ~angka();
+        void cetakData();
+        void isiData();
+};
+
+angka::angka(int i){
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+
 int main(){
-    
+
 }
