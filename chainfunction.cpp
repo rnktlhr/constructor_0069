798f50e8aca2dcbeb706c2d89382a6439c108afc
rnktlhr <rinakitluhur10@gmail.com>
2024-05-13 17:53:50 +0700

    menambahkan class buku serta menmber dan function

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -2,6 +2,19 @@
 #include <string>
 using namespace std;
 
+class buku{
+    string judul;
+
+    public:
+        buku setJudul(string judul){
+            this->judul = judul;
+            return *this;
+        }
+        string getJudul(){
+            return this->judul;
+        }
+}bukunya;
+
 int main(){
     
 }
